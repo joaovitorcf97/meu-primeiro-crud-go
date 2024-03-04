0135caaa487b2fa84b45151f8ace674c2c76653a
João vitor <joaovitorcfdev@gmail.com>
2024-03-03 23:58:57 -0300

    criando request e response de users

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joaovitorcf97/meu-primeiro-crud-go/src/controller/routes"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +14,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(os.Getenv("TEST"))
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
